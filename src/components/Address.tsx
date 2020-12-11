3b51b045f06656d837bcb4dfbbffe0d27dfa4beb
Trufa <trufavarela@gmail.com>
2020-12-11 01:04:57 +0100

    alt attribute

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -9,7 +9,7 @@ const Address: FC<Props> = ({ address }) => (
   <>
     <a className="text-blue-500 flex" rel={'noopener noreferrer'} target={'_blank'}
        href={`https://goerli.etherscan.io/address/${address}`}>
-      <img alt="identicon image" width="20" height="20"
+      <img alt="identicon" width="20" height="20"
            src={`data:image/png;base64,${getIdenticonData(address)}`}/>
       <div className="pl-3">{address.substr(0, 8)}...</div>
     </a>
