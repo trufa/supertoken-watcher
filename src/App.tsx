f7abcfb9397b053f2b25bf72da75c16700cbe81a
Trufa <trufavarela@gmail.com>
2020-12-11 02:06:03 +0100

    working with timestamp

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Address from './components/Address';
 import { useLiquidationQuery } from './generated/graphql';
+import { DateTime } from 'luxon';
+import LoadedComponent from './components/LoadedComponent';
+import { wad4human } from './helpers';
 
 const App = () => {
   const [{ data: { liquidations = [] } = {}, fetching, error }] = useLiquidationQuery({
-    variables: {
-      first: 5,
-    },
+    variables: {},
   });
   console.log(fetching, error);
   return (
@@ -16,27 +17,35 @@ const App = () => {
         <thead>
         <tr className="border-solid border-b border-gray-100">
           <th className="px-10 py-5">Id</th>
+          <th className="px-10 py-5">Timestamp</th>
           <th className="px-10 py-5">Penalty Account</th>
           <th className="px-10 py-5">Reward Account</th>
           <th className="px-10 py-5">Deposit</th>
         </tr>
         </thead>
         <tbody>
-        {
+        <LoadedComponent
+          fetching={fetching}
+          error={!!error}
+          fetchingComponent={<tr><td>Fetching</td></tr>}
+          errorComponent={<tr><td>Fetching</td></tr>}>
+          {
+            liquidations.map((liq) => (
+              <tr key={liq.id}>
+                <td className="px-10 py-4">{liq.id.substr(0, 8)}...</td>
+                <td className="px-10 py-4">{DateTime.fromSeconds(liq.timestamp).toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)}</td>
+                <td className="px-10 py-4">
+                  <Address address={liq.penaltyAccount}/>
+                </td>
+                <td className="px-10 py-4">
+                  <Address address={liq.rewardAccount}/>
+                </td>
+                <td className="px-10 py-4"><span className="text-green-500">◈</span>{wad4human(liq.deposit)}</td>
+              </tr>
+            ))
+          }
+        </LoadedComponent>
 
-          liquidations.map((liq) => (
-            <tr>
-              <td className="px-10 py-4">{liq.id.substr(0, 8)}...</td>
-              <td className="px-10 py-4">
-                <Address address={liq.penaltyAccount}/>
-              </td>
-              <td className="px-10 py-4">
-                <Address address={liq.rewardAccount}/>
-              </td>
-              <td className="px-10 py-4"><span className="text-green-500">◈</span>{liq.deposit / (10 ^ 18)}</td>
-            </tr>
-          ))
-        }
         </tbody>
       </table>
     </div>
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,71 @@
 import Identicon from 'identicon.js';
+import BN from 'bn.js';
 
 export const getIdenticonData = (hash: string): string => {
   return new Identicon(hash, 20).toString();
+};
+
+/**
+ * Get the whole and decimal parts from a number.
+ * Trims leading and trailing zeroes.
+ *
+ * @param {any} num the number
+ * @returns {Array<string>} array with the [<whole>, <decimal>] parts of the number
+ */
+function splitDecimalNumber(num: any) {
+  let sign = "";
+  if (typeof(num) == "number") {
+    // to avoid scientific notion (e-) of Number.toString()
+    // > 0.00000001.toString()
+    // '1e-8'
+    num = num.toFixed(50);
+  }
+  if (num.startsWith("-")) {
+    sign = "-";
+    num = num.slice(1);
+  }
+  const [whole = "", dec = ""] = num.toString().split(".");
+  return [
+    sign,
+    whole.replace(/^0*/, ""), // trim leading zeroes
+    dec.replace(/0*$/, ""), // trim trailing zeroes
+  ];
+}
+
+/**
+ * Format a decimal-based number back to a normal number
+ *
+ * @param {string} num the number
+ * @param {number} decimals number of decimal places
+ * @param {Object} [options] options object
+ * @param {bool} [options.truncate=true] Should the number be truncated to its decimal base
+ * @returns {string} formatted number
+ */
+function fromDecimals(num: string, decimals: number, { truncate = true } = {}) {
+  const [sign, whole, dec] = splitDecimalNumber(num);
+  if (!whole && !dec) {
+    return "0";
+  }
+
+  const paddedWhole = whole.padStart(decimals + 1, "0");
+  const decimalIndex = paddedWhole.length - decimals;
+  const wholeWithoutBase = paddedWhole.slice(0, decimalIndex);
+  const decWithoutBase = paddedWhole.slice(decimalIndex);
+
+  if (!truncate && dec) {
+    // We need to keep all the zeroes in this case
+    return `${sign}${wholeWithoutBase}.${decWithoutBase}${dec}`;
+  }
+
+  // Trim any trailing zeroes from the new decimals
+  const decWithoutBaseTrimmed = decWithoutBase.replace(/0*$/, "");
+  if (decWithoutBaseTrimmed) {
+    return `${sign}${wholeWithoutBase}.${decWithoutBaseTrimmed}`;
+  } else {
+    return sign + wholeWithoutBase;
+  }
+}
+
+export const wad4human = (wad: string, decimals = 5) => {
+  return Number(fromDecimals(wad.toString(), 18)).toFixed(decimals);
 }
